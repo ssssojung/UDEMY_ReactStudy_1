42e06e20470291bf8feb91802cee245398295e82
ssssojung <1216thwjd@naver.com>
2023-01-29 23:54:51 +0900

    78강

---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ChartBar from './ChartBar';
+import './Chart.css';
+
+const Chart = () => {}
+
+function ChartBar(props) {
+    return (
+        <div className='chart'>
+            {props.dataPoints.map(dataPoint => 
+                <ChartBar 
+                    key={dataPoint.label}
+                    value={dataPoint.value} 
+                    maxValue={null} 
+                    label={dataPoint.label} 
+                />
+            )}
+        </div>
+    );
+}
+
+export default ChartBar;
